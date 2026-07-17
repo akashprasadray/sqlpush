ebd747e1631409e35bfcbfd9c729ba94b35c6c46
Akash Prasad Ray <akashray3333@gmail.com>
2026-07-17 16:00:32 +0545

    initial commit

---
+++ college.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE IF NOT EXISTS college;
+USE college;
+
+-- CREATE TABLE
+CREATE TABLE student(
+roll_no INT PRIMARY KEY,
+name VARCHAR(50),
+marks INT 
+);
+
+-- INSERTING DATA INTO TABLE 
+INSERT INTO student 
+VALUES
+(1, "akash", 70),
+(2, "anish", 80);
+
+-- RETRIEVEING THE DATA
+SELECT * FROM student;
